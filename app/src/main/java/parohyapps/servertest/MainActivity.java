a9f0720dfba672d607f2756f7bc171fde5526727
Tomáš Paronai <tomas.parok@gmail.com>
2016-03-08 22:05:47 +0100

    update

--- app/src/main/java/parohyapps/servertest/MainActivity.java
+++ app/src/main/java/parohyapps/servertest/MainActivity.java
@@ -16,90 +16,114 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Reciever{
+
+    private TextView textOut = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textOut = (TextView) findViewById(R.id.tw_out);
-        String out = "";
+        textOut = (TextView) findViewById(R.id.tw_out);
 
-        ConnectThread connectThread = new ConnectThread(textOut);
+        ConnectThread connectThread = new ConnectThread(this);
         connectThread.execute();
 
+
+    }
+
+    @Override
+    public void recieve(String in) {
+        if(textOut != null){
+            JSONArray jsonArray = null;
+            try{
+                jsonArray = new JSONArray(in);
+                if(jsonArray.length() > 0){
+                    JSONObject jsonObject = (JSONObject) jsonArray.get(0);
+                    textOut.setText(jsonObject.getString("email"));
+                }
+            }
+            catch (Exception e){
+                Log.d("MainActivity","JSON error: "+e.toString());
+            }
+        }
     }
 
     private class ConnectThread extends AsyncTask<String,Void,String>{
 
-        private TextView textOut;
-        private boolean once = false;
-        public ConnectThread(TextView textOut){
-            this.textOut = textOut;
+        private Reciever reciever;
+
+        public ConnectThread(Reciever re){
+            reciever = re;
         }
+
 
         @Override
         protected String doInBackground(String... params) {
-            String out = "";
-            //if(!once){
-                //connect to db and get response
-                HttpClient httpClient = new DefaultHttpClient();
-                HttpPost httpPost = new HttpPost("http://192.168.1.4/serverTestBackend/getUser.php");
-                HttpResponse httpResponse = null;
-                HttpEntity httpEntity = null;
-                InputStream is = null;
-                try {
-                    httpResponse = httpClient.execute(httpPost);
-                    httpEntity = httpResponse.getEntity();
-                    is = httpEntity.getContent();
-                } catch (IOException e) {
-                    Log.d("MainActivity","Response error: "+e.toString());
-                }
 
-                //convert input from server
-                BufferedReader reader = null;
-                try{
-                    reader = new BufferedReader(new InputStreamReader(is,"iso-8859-1"),8);
-                    StringBuilder sb = new StringBuilder();
-                    String line = null;
-                    while((line = reader.readLine()) != null){
-                        Log.d("MainActivity","Appending: "+line);
-                        sb.append(line+"\n");
-                    }
-                    is.close();
-                    out = sb.toString();
-                    Log.d("MainActivity","Append result: "+out);
-                }
-                catch (Exception e){
-                    Log.d("MainActivity","Reader error: "+e.toString());
-                }
+            InputStream in = null;
+            String out = null;
+            //connect
+            try {
+                URL url = new URL("http://192.168.1.4/serverTestBackend/getUser.php");
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                in = new BufferedInputStream(urlConnection.getInputStream());
+            } catch (Exception e) {
+                Log.d("MainActivity","Connect error: "+e.toString());
+            }
 
-                //parse JSON
-                JSONArray jsonArray = null;
-                try{
-                    jsonArray = new JSONArray(out);
-                    if(jsonArray.length() > 0){
-                        JSONObject jsonObject = (JSONObject) jsonArray.get(0);
-                        textOut.setText(jsonObject.getString("email"));
-                    }
+            BufferedReader reader = null;
+            try{
+                reader = new BufferedReader(new InputStreamReader(in,"iso-8859-1"),8);
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+                while((line = reader.readLine()) != null){
+                    Log.d("MainActivity","Appending: "+line);
+                    sb.append(line+"\n");
                 }
-                catch (Exception e){
-                    Log.d("MainActivity","JSON error: "+e.toString());
-                }
-                once = true;
-           // }
+                in.close();
+                out = sb.toString();
+                Log.d("MainActivity","Append result: "+out);
+            }
+            catch (Exception e){
+                Log.d("MainActivity","Reader error: "+e.toString());
+            }
 
-            return null;
+            //parse JSON
+            /*JSONArray jsonArray = null;
+            try{
+                jsonArray = new JSONArray(out);
+                if(jsonArray.length() > 0){
+                    JSONObject jsonObject = (JSONObject) jsonArray.get(0);
+                    textOut.setText(jsonObject.getString("email"));
+                }
+            }
+            catch (Exception e){
+                Log.d("MainActivity","JSON error: "+e.toString());
+            }*/
+
+            return out;
         }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if(reciever != null){
+                reciever.recieve(s);
+            }
+        }
+
+
     }
 }
---
+++ app/src/main/java/parohyapps/servertest/Reciever.java
@@ -0,0 +1,8 @@
+package parohyapps.servertest;
+
+/**
+ * Created by tomas on 3/8/2016.
+ */
+public interface Reciever {
+    void recieve(String in);
+}
